4d99a404e7b12628ca66dc2ff9f09415e9c1a999
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-05-18 00:06:02 +0000

    Singleton lazy evaluation design pattern

---
+++ src/main/java/designpatterns/creational/singleton/lazyevaluation/Singleton.java
@@ -0,0 +1,24 @@
+package designpatterns.creational.singleton.lazyevaluation;
+
+public class Singleton {
+	//the private reference to the one and only instance
+	private static Singleton uniqueInstance = null;
+	//an instance attribute
+	private int data = 0;
+
+	private Singleton() {
+	}
+
+	public static Singleton getInstance() {
+		if (uniqueInstance == null) {
+			uniqueInstance = new Singleton();
+		}
+		return uniqueInstance;
+	}
+	public void setData(int myData){
+		data = myData;
+	}
+	public int getData() {
+		return data;
+	}
+}
---
+++ src/main/java/designpatterns/creational/singleton/lazyevaluation/TestSingleton.java
@@ -0,0 +1,16 @@
+package designpatterns.creational.singleton.lazyevaluation;
+
+public class TestSingleton {
+	public static void main(String[] args) {
+		Singleton s = Singleton.getInstance();
+		s.setData(55);
+		System.out.println("First reference: " + s);
+		System.out.println("Singleton data value is: " + s.getData());
+
+		//Get another reference to the singleton
+		s = null;
+		s = Singleton.getInstance();
+		System.out.println("First reference: " + s);
+		System.out.println("Secondary data value is: " + s.getData());
+	}
+}
