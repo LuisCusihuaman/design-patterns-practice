502a509ce728633161a53acbf7a44fff61e51e29
Luis Cusihuaman <luiscusihuaman88@gmail.com>
2020-05-18 00:16:12 +0000

    Singleton design pattern with synchronized method approach

---
+++ src/main/java/designpatterns/creational/singleton/synchronizedmethod/Singleton.java
@@ -0,0 +1,27 @@
+package designpatterns.creational.singleton.synchronizedmethod;
+
+public class Singleton {
+	//the private reference to the one and only instance
+	private static Singleton uniqueInstance = null;
+	//an instance attribute
+	private int data = 0;
+
+	private Singleton() {
+	}
+
+	//by adding the synchronized keyword to getInstance(),
+	// we force every thread to wait its turn before it can enter the method
+	public static synchronized Singleton getInstance() {
+
+		if (uniqueInstance == null) {
+			uniqueInstance = new Singleton();
+		}
+		return uniqueInstance;
+	}
+	public void setData(int myData){
+		data = myData;
+	}
+	public int getData() {
+		return data;
+	}
+}
